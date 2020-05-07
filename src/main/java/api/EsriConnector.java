9d7d9065512fd4174b57ba4810c9aaa4fa645239
Ibraheem-A <43247583+Ibraheem-A@users.noreply.github.com>
2020-05-07 17:55:48 +0200

    added Esri connector to process API requests

---
+++ src/main/java/api/EsriConnector.java
@@ -0,0 +1,84 @@
+package api;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.time.LocalDate;
+
+import org.json.*;
+
+public class EsriConnector {
+
+    public Location getCoordinatesFromServer(String address){
+        String url = "http://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer/" +
+                "findAddressCandidates?singleLine="+ address + "&forStorage=false&outSR=5684&f=pjson";
+        String json = downloadJsonFromServer(url);
+        double[] coord = getCoordinatesFromJson(json);
+
+        return new Location(address, coord[0], coord[1]);
+    }
+
+    private String downloadJsonFromServer(String... strings){
+        StringBuilder json = new StringBuilder();
+        URL url;
+        HttpURLConnection httpURLConnection;
+        try{
+            url = new URL(strings[0]);
+            httpURLConnection = (HttpURLConnection) url.openConnection();
+            InputStream in = httpURLConnection.getInputStream();
+            InputStreamReader reader = new InputStreamReader(in);
+            int data = reader.read();
+            while (data != -1){
+                char current = (char)data;
+                json.append(current);
+                data = reader.read();
+            }
+            return json.toString();
+        } catch (IOException e){
+            e.printStackTrace();
+            return "Failed";
+        }
+    }
+    
+    private double[] getCoordinatesFromJson(String json){
+        double x, y;
+
+        JSONObject jsonObject = new JSONObject(json);
+        String candidates = jsonObject.getString("candidates");
+        JSONArray candidateArray = new JSONArray(candidates);
+        JSONObject partObject = candidateArray.getJSONObject(0);
+        String location = partObject.getString("location");
+        JSONObject locationObject = new JSONObject(location);
+
+        x = locationObject.getDouble("x");
+        y = locationObject.getDouble("y");
+
+        return new double[] {x, y};
+    }
+
+    class Location{
+        private String address;
+        private double x;
+        private double y;
+
+        public Location(String address, double x, double y){
+            this.address = address;
+            this.x = x;
+            this.y = y;
+        }
+
+        public String getAddress() {
+            return address;
+        }
+
+        public double getX() {
+            return x;
+        }
+
+        public double getY() {
+            return y;
+        }
+    }
+}
