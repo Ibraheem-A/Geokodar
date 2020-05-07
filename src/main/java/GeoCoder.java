4a392e17b3aa5139a80a0d4f0a5a61f788ac9bd4
Ibraheem-A <43247583+Ibraheem-A@users.noreply.github.com>
2020-05-07 18:26:26 +0200

    moved file handling methods to FileReader class

--- src/main/java/GeoCoder.java
+++ src/main/java/GeoCoder.java
@@ -14,36 +14,4 @@ public class GeoCoder {
     private void geocodeAddress(){
 
     }
-
-    private ArrayList<String> getAddressesFromCsv(String path, int columnIndex){
-        ArrayList<String> addressColumn= new ArrayList<>();
-        try (FileReader fileReader = new FileReader(new File(path));
-             BufferedReader csvReader = new BufferedReader(fileReader)){
-            String row;
-            while (!(row = csvReader.readLine()).equals(null)){
-                addressColumn.add(row.split(",")[columnIndex]);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return addressColumn;
-    }
-
-    private ArrayList<String> getAddressFromXlsx(String path, String sheetName, int columnIndex){
-        ArrayList<String> addressColumn= new ArrayList<>();
-        try (FileInputStream fileInputStream = new FileInputStream(new File(path));
-             XSSFWorkbook workbook = new XSSFWorkbook(fileInputStream)){
-            XSSFSheet sheet = workbook.getSheet(sheetName);
-            while (sheet.iterator().hasNext()){
-                Row currentRow = sheet.iterator().next();
-                Cell addressCell = currentRow.getCell(columnIndex);
-                String address = addressCell.getStringCellValue();
-                addressColumn.add(address);
-            }
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return addressColumn;
-    }
 }
---
+++ src/main/java/util/FileReader.java
@@ -0,0 +1,45 @@
+package util;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.*;
+import java.util.ArrayList;
+
+public class FileReader {
+
+
+    public ArrayList<String> getAddressesFromCsv(String path, int addressColumnIndex){
+        ArrayList<String> addressColumn= new ArrayList<>();
+        try (java.io.FileReader fileReader = new java.io.FileReader(new File(path));
+             BufferedReader csvReader = new BufferedReader(fileReader)){
+            String row;
+            while (!(row = csvReader.readLine()).equals(null)){
+                addressColumn.add(row.split(",")[addressColumnIndex]);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return addressColumn;
+    }
+
+    public ArrayList<String> getAddressFromXlsx(String path, String sheetName, int addressColumnIndex){
+        ArrayList<String> addressColumn= new ArrayList<>();
+        try (FileInputStream fileInputStream = new FileInputStream(new File(path));
+             XSSFWorkbook workbook = new XSSFWorkbook(fileInputStream)){
+            XSSFSheet sheet = workbook.getSheet(sheetName);
+            while (sheet.iterator().hasNext()){
+                Row currentRow = sheet.iterator().next();
+                Cell addressCell = currentRow.getCell(addressColumnIndex);
+                String address = addressCell.getStringCellValue();
+                addressColumn.add(address);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return addressColumn;
+    }
+}
